1bfa0e8c77718bed923ec9bf1a84c980b29e25ea
陳柏勳 <3B032013@gm.student.ncut.edu.tw>
2023-10-27 11:21:57 +0800

    設定Controller回傳對應的view & 根路由轉向posts.index路由

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('posts.index');
+    return redirect(route('posts.index'));
 });
 
 Route::get('posts',[PostController::class, 'index'])->name('posts.index');
